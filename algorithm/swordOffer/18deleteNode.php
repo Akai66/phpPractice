001c4206f7bb7d8aabfb623b11c4ca078e334cf6
liukai <869896679@qq.com>
2020-04-16 11:42:40 +0800

    链表删除节点

---
+++ algorithm/swordOffer/18deleteNode.php
@@ -0,0 +1,33 @@
+<?php
+require_once "linkNode.php";
+
+/**
+ * 要求O(1)时间复杂度删除单链表中的指定节点
+ */
+
+
+function deleteNode(&$head,$target){
+   if(!empty($target->next)){
+       $next = $target->next;
+       $target->value = $next->value;
+       $target->next= $next->next;
+   }elseif($head == $target){
+       //链表只有一个节点,删除头结点
+       $head = null;
+   }else{
+       //链表中有多个节点,删除尾结点
+       $node = $head;
+       while($node->next != $target){
+           $node = $node->next;
+       }
+       $node->next = null;
+   }
+}
+
+$head = new linkNode(1);
+$node2 = new linkNode(2);
+$node3 = new linkNode(3);
+$head->next = $node2;
+$node2->next = $node3;
+deleteNode($head,$node2);
+var_dump($head);
--- algorithm/swordOffer/6_2removeNode.php
+++ algorithm/swordOffer/6_2removeNode.php
@@ -7,25 +7,18 @@ require_once "linkNode.php";
 
 function removeNode(&$head,$target){
     if(empty($head)){
-        echo "参数错误:链表为空";
-        return;
+        throw new Exception("参数错误:链表为空");
     }
-    $delNode = null;
-    if($head->value == $target){
-        $delNode = $head;
+    if($head == $target){
         $head = $head->next;
     }else{
         $node = $head;
-        while(!empty($node->next) && $node->next->value != $target){
+        while(!empty($node->next) && $node->next != $target){
             $node = $node->next;
         }
         if(!empty($node->next)){
-            $delNode = $node->next;
             $node->next = $node->next->next;
         }
-    }
-    if(!empty($delNode)){
-        unset($delNode);
     }
 }
 
@@ -34,5 +27,5 @@ $node2 = new linkNode(2);
 $node3 = new linkNode(3);
 $head->next = $node2;
 $node2->next = $node3;
-removeNode($head,2);
+removeNode($head,$node2);
 var_dump($head);
